d43922a6d084b66f06dc0b9cb786e934026e6980
Stone <74167978+s71557@users.noreply.github.com>
2024-03-19 22:11:46 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -210,7 +210,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://sub.shizongliang.cn/sub?" }],
+        backendOptions: [{ value: "http://sub.shizongliang.cn/sub?" },{ value: "http://127.0.0.1:25500/sub?" }],
         remoteConfig: [
           {
             label: "universal",
