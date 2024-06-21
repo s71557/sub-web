a8ce8150749b971ccdc598fb7e8b1ed0c0947948
Stone <74167978+s71557@users.noreply.github.com>
2024-06-21 11:19:01 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -28,7 +28,7 @@
               <div v-if="advanced === '2'">
                 <el-form-item label="后端地址:">
                   <el-autocomplete style="width: 100%" v-model="form.customBackend" :fetch-suggestions="backendSearch"
-                    placeholder="动动小手，（建议）自行搭建后端服务。例：https://sub.715577.xyz/sub?">
+                    placeholder="动动小手，（建议）自行搭建后端服务。例：https://sub.shizongliang.cn/sub?">
                     <el-button slot="append" @click="gotoGayhub" icon="el-icon-link">前往项目仓库</el-button>
                   </el-autocomplete>
                 </el-form-item>
@@ -212,7 +212,7 @@ export default {
           Trojan: "trojan",
           Surge3: "surge&ver=3",
         },
-        backendOptions: [{ value: "https://sub.715577.xyz/sub?" },{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://sub.shizongliang.cn/sub?" },{ value: "http://127.0.0.1:25500/sub?" }],
         remoteConfig: [
           {
             label: "universal",
